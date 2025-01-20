7af666ee0a10e021ce23da83c1988b1fe955e775
Deepanjali Dilish <0006deepanjalidilishvft@gmail.com>
2025-01-20 10:37:09 +0530

    final commit

--- script.js
+++ script.js
@@ -1,300 +1,174 @@
-// const ballContainer = document.getElementById('ball-container')
-// const ballColors = getRandomColors(15)
+document.getElementById('letsPlayBtn').addEventListener('click', function() {
+    document.querySelector('.background-container').style.display = 'none';
+    document.getElementById('instructionPage').style.display = 'block';
+});
 
-// let selectedJar = null
-// let selectedBall = null
-// const jars = { 1: [], 2: [], 3: []}
-// const maxBall = 5
-
-
-
-// for (let i=1; i<= 15; i++){
-//     const ball = document.createElement('div')
-//     ball.classList.add('ball')
-//     ball.id = `balls${i}`
-//     ball.textContent = i
-//     ball.style.backgroundColor = ballColors[i - 1]
-
-//     ball.onclick = () => selectBall(i)
-//     ballContainer.appendChild(ball)
-// }
-
-// function getRandomColors(count){
-//     const colors = []
-//     for(let i=0; i<count; i++){
-//         colors.push(randomColors())
-//     }
-//     return colors
-// }
-
-// function randomColors(){
-//     const codes = "0123456789ABCDEF"
-//     let color = "#"
-//     for(let i=0; i<6; i++){
-//         color += codes[Math.floor(Math.random() * 16)]
-//     }
-//     return color
-// }
-
-// function selectBall(ballNumber){
-//     if(ballInJar(ballNumber)) {
-
-//         removeBall(ballNumber)
-//         return
-//     }
-
-//     selectedBall = ballNumber
-// }
-
-// function ballInJar(ballNumber){
-//     return Object.values(jars).some(jar => jar.includes(ballNumber))
-// }
-
-// function selectJar(jarNumber){
-//     if(ballInJar(selectedBall)){
-//         return
-//     }
-
-//     if(selectedBall != null){
+document.getElementById('startGameBtn').addEventListener('click', function() {
     
-//         jars[jarNumber].push(selectedBall)
-//         arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber)
-//         console.log(`Ball ${selectedBall} stored in Jar ${jarNumber}`)
-//         selectedBall = null
-//         return
-//     }
+    document.body.style.backgroundColor = 'black';
+    document.body.style.backgroundImage = 'none'; 
 
-//     if(selectedJar === null){
-//         if(jars[jarNumber].length === 0){
-//             return
-//         }
-//         selectedJar = jarNumber
-//         console.log(`Selected Jar ${selectedJar}`)
-//         return
-//     }
-//     moveBall(selectedJar, jarNumber)
-//     selectedJar = null
-// }
+    document.getElementById('instructionPage').style.display = 'none';
+    document.querySelector('.container').style.display = 'flex';
+    document.querySelector('.icon-container').style.display = 'block';
 
-// function arrangeBalls(jarElement, jarNumber){
+    const ballContainer = document.getElementById('ball-container');
+    const ballColors = getRandomColors(10);
 
-//     const ballHeight = 45
-//     const jarHeight = 232
+    for (let i = 1; i <= 10; i++) {
+        const ball = document.createElement('div');
+        ball.classList.add('ball');
+        ball.id = `balls${i}`;
+        ball.textContent = i;
+        ball.style.backgroundColor = ballColors[i - 1];
+        ball.onclick = () => selectBall(i);
+        ballContainer.appendChild(ball);
+    }
+});
 
+document.getElementById('instructionIcon').addEventListener('click', function() {
 
-//     const numBalls = jars[jarNumber].length
-//     const newHeight = Math.max(jarHeight, ballHeight * numBalls + 20)
+    const overlayContent = document.querySelector('.overlay-instruction-content');
 
-//     jarElement.style.height = `${newHeight}px`
+    overlayContent.innerHTML = '';
 
-//     jars[jarNumber].forEach((ballNumber, index) => {
-//         const ballElement = document.getElementById(`balls${ballNumber}`)
-//         jarElement.appendChild(ballElement)
-//         ballElement.style.margin = "0"
-//         ballElement.style.position = "absolute"
-//         ballElement.style.height = "40px"
-//         ballElement.style.width = "40px"
-//         ballElement.style.left = "50%"
-//         ballElement.style.transform = "translateX(-50%)"
-//         ballElement.style.bottom = `${index * ballHeight}px`
-//     })
-// }
+    const heading = document.createElement('h1');
+    heading.textContent = 'Game Instructions';
 
-// function moveBall(fromJar, tojar){
-//     const move = jars[fromJar].pop()
-//     if (!move){
-//         return
-//     }
+    const paragraph = document.createElement('p');
 
-//     jars[tojar].push(move)
-//     arrangeBalls(document.getElementById(`jar${fromJar}`), fromJar)
-//     arrangeBalls(document.getElementById(`jar${tojar}`), tojar)
-//     console.log(`Moved Ball ${move} from Jar ${fromJar} to Jar ${tojar}` )
-// }
+    paragraph.textContent = `1)Ball to Jar: Click a ball, then select a jar to move it there.<br><br>2)Jar to Jar: Click a jar, then click another jar to move the top ball.<br><br>3)Remove Ball: Click any ball in a jar to remove it.`;
+    paragraph.innerHTML = paragraph.textContent; 
 
-// function removeBall(ballNumber){
-//     for(const jarNumber in jars){
-//         const jarIndex = jars[jarNumber].indexOf(ballNumber)
+    overlayContent.appendChild(heading);
+    overlayContent.appendChild(paragraph);
 
-//         if(jarIndex !== -1){
-//             jars[jarNumber].splice(jarIndex, 1)
+    document.getElementById('overlayInstructionPage').style.display = 'flex';
 
-//             arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber)
+    const closeButton = document.createElement('button');
+    closeButton.textContent = 'Close';
+    closeButton.classList.add('btn'); 
+    overlayContent.appendChild(closeButton);
 
-//             const ballElement = document.getElementById(`balls${ballNumber}`)
-//             ballElement.style.margin = "5px"
-//             ballElement.style.position = "static"
-//             ballElement.style.height = "50px"
-//             ballElement.style.width = "50px"
-//             ballElement.style.transform = "none"
+    closeButton.addEventListener('click', function() {
+        document.getElementById('overlayInstructionPage').style.display = 'none';
+    });
+});
 
-//             ballOrder(ballElement)
-//             console.log(`Ball ${ballNumber} removed from Jar ${jarNumber}`)
-//             break
-//         }
-//     }
-// }
+let selectedJar = null;
+let selectedBall = null;
+const jars = { 1: [], 2: [], 3: [] };
+const maxBall = 5;
 
-// function ballOrder(ballElement){
-//     const ballNumber = parseInt(ballElement.textContent)
-//     const balls = Array.from(ballContainer.children)
+function getRandomColors(count) {
+    const colors = [];
+    for (let i = 0; i < count; i++) {
+        colors.push(randomColors());
+    }
+    return colors;
+}      
 
-//     let inserted = false
-//     for(let i=0; i<balls.length;i++){
-//         if(parseInt(balls[i].textContent) > ballNumber){
-//             ballContainer.insertBefore(ballElement, balls[i])
-//             inserted = true
-//             break
-//         }
-//     }
+function randomColors() {
+    const codes = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += codes[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}       
 
-//     if (!inserted){
-//         ballContainer.appendChild(ballElement)
-//     }
-// }
+function selectBall(ballNumber) {
+    if (ballInJar(ballNumber)) {
+        removeBall(ballNumber);
+        return;
+    }
 
+    selectedBall = ballNumber;
+}      
 
-
-const ballContainer = document.getElementById('ball-container');
- const ballColors = getRandomColors(15);
-
-        let selectedJar = null;
-        let selectedBall = null;
-        const jars = { 1: [], 2: [], 3: [] };
-        const maxBall = 5;
-
-        // Generate balls
-        for (let i = 1; i <= 15; i++) {
-            const ball = document.createElement('div');
-            ball.classList.add('ball');
-            ball.id = `balls${i}`;
-            ball.textContent = i;
-            ball.style.backgroundColor = ballColors[i - 1];
-
-            ball.onclick = () => selectBall(i);
-            ballContainer.appendChild(ball);
-        }
-
-        function getRandomColors(count) {
-            const colors = [];
-            for (let i = 0; i < count; i++) {
-                colors.push(randomColors());
-            }
-            return colors;
-        }
-
-        function randomColors() {
-            const codes = "0123456789ABCDEF";
-            let color = "#";
-            for (let i = 0; i < 6; i++) {
-                color += codes[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
-
-        function selectBall(ballNumber) {
-            if (ballInJar(ballNumber)) {
-                removeBall(ballNumber);
-                return;
-            }
-
-            selectedBall = ballNumber;
-        }
-
+function ballInJar(ballNumber) {
+    return Object.values(jars).some(jar => jar.includes(ballNumber));
+}
+function selectJar(jarNumber) {
+    if (selectedBall !== null) {
         
-
-        function ballInJar(ballNumber) {
-            return Object.values(jars).some(jar => jar.includes(ballNumber));
+        if (jars[jarNumber].length >= maxBall) {
+            alert("You cannot add more than 5 balls to a jar!");
+            return; 
         }
 
-        function selectJar(jarNumber) {
-            if (selectedBall != null) {
-                if (jars[jarNumber].length >= maxBall) {
-                    alert("You cannot add more than 5 balls to a jar!");
-                    return;
-                }
+        jars[jarNumber].push(selectedBall);
+        arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber);
+        console.log(`Ball ${selectedBall} stored in Jar ${jarNumber}`);
+        selectedBall = null;
+        return;
+    }
 
-                jars[jarNumber].push(selectedBall);
-                arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber);
-                console.log(`Ball ${selectedBall} stored in Jar ${jarNumber}`);
-                selectedBall = null;
-                return;
-            }
-
-            if (selectedJar === null) {
-                if (jars[jarNumber].length === 0) {
-                    return;
-                }
-                selectedJar = jarNumber;
-                console.log(`Selected Jar ${selectedJar}`);
-                return;
-            }
-
-            moveBall(selectedJar, jarNumber);
-            selectedJar = null;
+    if (selectedJar === null) {
+        if (jars[jarNumber].length === 0) {
+            return;
         }
+        selectedJar = jarNumber;
+        console.log(`Selected Jar ${selectedJar}`);
+        return;
+    }
 
-        function arrangeBalls(jarElement, jarNumber) {
-            jars[jarNumber].forEach((ballNumber, index) => {
-                const ballElement = document.getElementById(`balls${ballNumber}`);
-                jarElement.appendChild(ballElement);
-                ballElement.style.margin = "0";
-                ballElement.style.position = "absolute";
-                ballElement.style.height = "40px";
-                ballElement.style.width = "40px";
-                ballElement.style.left = "50%";
-                ballElement.style.transform = "translateX(-50%)";
-                ballElement.style.bottom = `${index * 45}px`;
-            });
-        }
+    moveBall(selectedJar, jarNumber);
+    selectedJar = null;
+}
 
+
+function arrangeBalls(jarElement, jarNumber) {
+    jars[jarNumber].forEach((ballNumber, index) => {
+        const ballElement = document.getElementById(`balls${ballNumber}`);
+        jarElement.appendChild(ballElement);
+        ballElement.style.margin = "0";
+        ballElement.style.position = "absolute";
+        ballElement.style.height = "40px";
+        ballElement.style.width = "40px";
+        ballElement.style.left = "50%";
+        ballElement.style.transform = "translateX(-50%)";
+        ballElement.style.bottom = `${index * 45}px`;
+    });
+}
+
+function moveBall(fromJar, toJar) {
+
+    if (jars[toJar].length >= maxBall) {
+        alert("The jar is already full. To add more balls to it, remove a ball from the jar and then add the new ball.");
+        return;
+    }
+
+    const move = jars[fromJar].pop();
+    if (!move) {
+        return;
+    }
+
+    jars[toJar].push(move);
+    arrangeBalls(document.getElementById(`jar${fromJar}`), fromJar);
+    arrangeBalls(document.getElementById(`jar${toJar}`), toJar);
+    console.log(`Moved Ball ${move} from Jar ${fromJar} to Jar ${toJar}`);
+}
+
+function removeBall(ballNumber) {
+    for (const jarNumber in jars) {
+        const jarIndex = jars[jarNumber].indexOf(ballNumber);
+
+        if (jarIndex !== -1) {
         
-        function moveBall(fromJar, toJar) {
-            // Prevent moving if the destination jar already has 5 balls
-            if (jars[toJar].length >= maxBall) {
-                alert("The destination jar already has 5 balls! Movement not allowed.");
-                return;
-            }
-        
-            // Move the ball from the source jar to the destination jar
-            const move = jars[fromJar].pop();
-            if (!move) {
-                return;
-            }
-        
-            jars[toJar].push(move);
-            arrangeBalls(document.getElementById(`jar${fromJar}`), fromJar);
-            arrangeBalls(document.getElementById(`jar${toJar}`), toJar);
-            console.log(`Moved Ball ${move} from Jar ${fromJar} to Jar ${toJar}`);
-        }
-        
-       
+            jars[jarNumber].splice(jarIndex, 1);
 
-        function removeBall(ballNumber) {
-            for (const jarNumber in jars) {
-                const jarIndex = jars[jarNumber].indexOf(ballNumber);
+    
+            arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber);
 
-                if (jarIndex !== -1) {
-                    jars[jarNumber].splice(jarIndex, 1);
+            const ballElement = document.getElementById(`balls${ballNumber}`);
+            ballElement.style.margin = "5px";
+            ballElement.style.position = "static";
+            ballElement.style.height = "50px";
+            ballElement.style.width = "50px";
+            ballElement.style.transform = "none";
 
-                    arrangeBalls(document.getElementById(`jar${jarNumber}`), jarNumber);
-
-                    const ballElement = document.getElementById(`balls${ballNumber}`);
-                    ballElement.style.margin = "5px";
-                    ballElement.style.position = "static";
-                    ballElement.style.height = "50px";
-                    ballElement.style.width = "50px";
-                    ballElement.style.transform = "none";
-
-                    ballOrder(ballElement);
-                    console.log(`Ball ${ballNumber} removed from Jar ${jarNumber}`);
-                    break;
-                }
-            }
-        }
-
-        function ballOrder(ballElement) {
-            const ballNumber = parseInt(ballElement.textContent);
+            const ballContainer = document.getElementById('ball-container');
             const balls = Array.from(ballContainer.children);
 
             let inserted = false;
@@ -309,7 +183,9 @@ const ballContainer = document.getElementById('ball-container');
             if (!inserted) {
                 ballContainer.appendChild(ballElement);
             }
+
+            console.log(`Ball ${ballNumber} removed from Jar ${jarNumber} and added back to the container`);
+            break;
         }
-
-
-
+    }
+}
